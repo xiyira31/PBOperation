0a9f2a0691383aa085e50103005079988b6d003b
xiyira31 <xiyira31@gmail.com>
2017-11-14 15:25:13 +0800

    添加对nedb的支持

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -6,11 +6,15 @@ import App from './App'
 import router from './router'
 import store from './store'
 
+import Nedb from 'nedb'
+
 Vue.use(Vuetify)
 if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
 Vue.http = Vue.prototype.$http = axios
 Vue.config.productionTip = false
 
+const db = new Nedb({filename: 'data/datafile', autoload: true})
+Vue.db = Vue.prototype.$db = db
 /* eslint-disable no-new */
 new Vue({
   components: { App },
